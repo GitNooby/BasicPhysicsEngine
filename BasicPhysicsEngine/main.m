928b60091d7d01f115d899ff92d4bdb353f23705
Kai Zou <kaizou898@gmail.com>
2014-05-15 19:55:21 -0400

    Initial Commit

---
+++ BasicPhysicsEngine/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  BasicPhysicsEngine
+//
+//  Created by Kai Zou on 2014-05-15.
+//  Copyright (c) 2014 com.personal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
